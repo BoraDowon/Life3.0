c7e3b5f628254095711b139536d41c9eb209582d
akahard2dj <akahard2dj@naver.com>
2017-10-25 11:03:37 +0900

    New DB Model for LifeLog
    - User item is not defined -> TBD
    - STATUS_INFO, TYPE_INFO : element check is needed in a class function( @property?, def __init__ ?)
    - Meta class will be implemented for a qeurying

--- life3/app/models.py
+++ life3/app/models.py
@@ -1,3 +1,28 @@
+import time
+
 from django.db import models
 
-# Create your models here.
+
+class LifeLog(models.Model):
+    STATUS_INFO = (
+        ('0', 'Active'),
+        ('1', 'Inactive'),
+    )
+
+    TYPE_INFO = (
+        ('W', 'Work'),
+        ('P', 'Play'),
+        ('R', 'Rest'),
+    )
+
+    title = models.CharField(max_length=500)
+    timestamp = models.IntegerField(default=(time.time()))
+    status = models.CharField(max_length=1, choices=STATUS_INFO)
+    type = models.CharField(max_length=1, choices=TYPE_INFO)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return 'LifeLog Model : {}, {}, {}, {}'.\
+            format(self.title, self.timestamp, self.type, self.status)
