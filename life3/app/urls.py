1e46cfd97bbc7db96d57be3497cd51f43c4d5f52
Dowon <blessjdw@gmail.com>
2017-10-28 15:29:41 +0900

    removed sample code

--- life3/app/urls.py
+++ life3/app/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^articles', views.api_home_articles),
     url(r'^life-logs', views.api_life_logs),
 ]
--- life3/app/views.py
+++ life3/app/views.py
@@ -11,21 +11,6 @@ def api_home(request):
     return render(request, 'home.html')
 
 
-# TODO: remove
-def api_home_articles(request):
-    posts = []
-    a1 = {'id': 1, 'title': 'test1'}
-    a2 = {'id': 2, 'title': 'test2'}
-    a3 = {'id': 3, 'title': 'test3'}
-
-    posts.append(a1)
-    posts.append(a2)
-    posts.append(a3)
-
-    posts_map = {'articles': posts}
-    return JsonResponse(posts_map)
-
-
 def api_life_logs(request: HttpRequest) -> JsonResponse:
     """ API for life logs
 
