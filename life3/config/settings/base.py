7b8cff6dbc8c81d9566e31afa4e219501c2bf0f4
akahard2dj <akahard2dj@naver.com>
2017-10-30 10:18:17 +0900

    csrf enable

--- life3/config/settings/base.py
+++ life3/config/settings/base.py
@@ -41,7 +41,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # due to POST method... 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
