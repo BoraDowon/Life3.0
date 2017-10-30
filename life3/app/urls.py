6c2514c1152a22dbebc8ce8d371c70f6ad1886d3
akahard2dj <akahard2dj@naver.com>
2017-10-30 16:07:53 +0900

    class view applying

--- life3/app/urls.py
+++ life3/app/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^lifecards', views.api_lifecards),
+    url(r'^lifecards/$', views.LifeCardList.as_view()),
+    #url(r'^lifecards/(?P<pk>[0-9]+)/$', views.LifeCardDetail.as_view()),
+    #url(r'^lifecards', views.api_lifecards),
     url(r'^stats', views.api_statistics),
 ]
