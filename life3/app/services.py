b8402cc26181d25e0dca1f859fc3703a247bce10
akahard2dj <akahard2dj@naver.com>
2017-10-30 10:54:38 +0900

    read_events only queries that has an active status

--- life3/app/services.py
+++ life3/app/services.py
@@ -2,6 +2,8 @@ from typing import List
 from datetime import datetime
 import pytz
 
+from django.core.exceptions import ObjectDoesNotExist
+
 from .models import LifeCard
 
 # TODO: do we need to consider class based?
@@ -29,11 +31,38 @@ def create_event(data: dict) -> bool:
     return True
 
 
-def remove_event():
+def remove_event(pk) -> bool:
+    '''
+    try:
+        lifecard = LifeCard.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return False
+
+    lifecard.status = '1'
+    lifecard.save()
+    return True
+    '''
     pass
 
 
-def modify_event():
+def modify_event(data: dict) -> bool:
+    '''
+    try:
+        lifecard = LifeCard.objects.get(pk=data['id'])
+    except ObjectDoesNotExist:
+        return False
+
+    type_info = LifeCard.TYPE_INFO
+    type_validation = [i for i, v in enumerate(type_info) if v[0] == data['type']]
+    if type_validation:
+        lifecard.type = data['type']
+    else:
+        return False
+
+    lifecard.title = data['title']
+    lifecard.save()
+    return True
+    '''
     pass
 
 
@@ -43,8 +72,9 @@ def read_events() -> List[dict]:
     :return: list of events
     """
     # TODO: in future, the function will get a information of timezone
-    query = LifeCard.objects.all().values('id', 'title', 'type', 'timestamp')
-    return list(map(_deco_time_to_display, list(query)))
+    query = LifeCard.objects.all().values('id', 'title', 'type', 'status', 'timestamp')
+    active_lifecard = [item for item in list(query) if item['status'] == '0']
+    return list(map(_deco_time_to_display, active_lifecard))
 
 
 def _deco_time_to_display(life_card: dict) -> dict:
@@ -53,6 +83,7 @@ def _deco_time_to_display(life_card: dict) -> dict:
     timezone = pytz.timezone('Asia/Seoul')
     life_card['time_to_display'] = life_card['timestamp'].astimezone(tz=timezone).strftime('%Y-%m-%d %H:%M')
     del life_card['timestamp']
+    del life_card['status']
     return life_card
 
 
