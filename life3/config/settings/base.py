509829e0b61257ec6a172de05fa00d5286ea8a74
akahard2dj <akahard2dj@naver.com>
2017-10-30 16:07:27 +0900

    applying django-rest-framwork

--- life3/config/settings/base.py
+++ life3/config/settings/base.py
@@ -34,7 +34,8 @@ INSTALLED_APPS = [
     #   'django.contrib.sessions',
     #   'django.contrib.messages',
     'django.contrib.staticfiles',
-    'life3.app'
+    'rest_framework',
+    'life3.app',
 ]
 
 MIDDLEWARE = [
