93c9767fb5f905eba4340f8fbbda4f922368f6c1
akahard2dj <akahard2dj@naver.com>
2017-10-31 15:13:48 +0900

    production database

--- life3/config/settings/production.py
+++ life3/config/settings/production.py
@@ -1,3 +1,13 @@
+import os
 from .base import *
 
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DATABASE_NAME'],
+        'USER': os.environ['DATABASE_USER'],
+        'PASSWORD': os.environ['DATABASE_PASS'],
+    }
+}
