042dc7ae4c2db8d74062a5b636748e99346e1aba
akahard2dj <akahard2dj@naver.com>
2017-10-31 13:17:13 +0900

    applying a life_card detail service

--- life3/app/urls.py
+++ life3/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^lifecards/$', views.LifeCardList.as_view()),
-    #url(r'^lifecards/(?P<pk>[0-9]+)/$', views.LifeCardDetail.as_view()),
+    url(r'^lifecards/(?P<card_pk>[0-9]+)/$', views.LifeCardDetail.as_view()),
     #url(r'^lifecards', views.api_lifecards),
     url(r'^stats', views.api_statistics),
 ]
