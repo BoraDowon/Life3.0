4d0e9d7826c404084baa3cbc00bbced2f6fe7319
Dowon Jeong <blessjdw@gmail.com>
2017-11-05 14:07:24 +0900

    implemented login api view

--- life3/login/views.py
+++ life3/login/views.py
@@ -1,8 +1,42 @@
+from django.http import HttpRequest
 from django.views.generic import TemplateView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from life3.user.user_service import UserService
 
 
 class LoginHome(TemplateView):
+    """
+    Login template
+    """
     template_name = 'login.html'
 
 
+class LoginApi(APIView):
 
+    def post(self, request: HttpRequest):
+        """
+        Create user from profile based on OAuth.
+        If user already exists or token has expired, update user from profile
+
+        :param request: HttpRequest
+        :return: JSON response
+        """
+
+        user_service = UserService()
+
+        message = 'success'
+        profile = request.GET['profile']
+        if profile['token']:
+            if user_service.is_valid_user(profile['token']):
+                user_service.update_user_profile(profile)
+            else:
+                user_service.create_user_profile(profile)
+        else:
+            message = 'token is not included!'
+
+        return Response({'result', message})
+
+    def get(self):
+        return Response({'result', 'unsupported request'})
---
+++ life3/user/user_service.py
@@ -0,0 +1,12 @@
+# TODO: implement
+class UserService:
+    def is_valid_user(self, token: str):
+        pass
+
+    # TODO: consider using obj instead dict
+    def update_user_profile(self, profile: dict):
+        pass
+
+    # TODO: consider using obj instead dict
+    def create_user_profile(self, profile: dict):
+        pass
