f6f225df5ca49d5970390ab2f8d50cd130575eed
akahard2dj <akahard2dj@naver.com>
2017-11-06 11:30:51 +0900

    code cleans

--- life3/dashboard/models.py
+++ life3/dashboard/models.py
@@ -1,6 +1,39 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
 
+
+class LifeCard(models.Model):
+    STATUS_INFO = (
+        ('0', 'Active'),
+        ('1', 'Inactive'),
+    )
+
+    TYPE_INFO = (
+        ('W', 'Work'),
+        ('P', 'Play'),
+        ('R', 'Rest'),
+    )
+
+    title = models.CharField(max_length=500)
+    timestamp = models.DateTimeField()
+    status = models.CharField(max_length=1, choices=STATUS_INFO)
+    type = models.CharField(max_length=1, choices=TYPE_INFO)
+
+    # account ID which is same as account_id in life3.user.models.LifeUser
+    account_id = models.CharField(max_length=30, null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'LifeCard Model : {}, {}, {}, {}'.\
+            format(self.title, self.timestamp, self.type, self.status)
+
+    class Meta:
+        ordering = ['-timestamp']
+
+
+# TODO TBD: UserModel
+from django.contrib.auth.models import AbstractBaseUser
 from .managers import LifeUserManager
 
 
@@ -36,34 +69,3 @@ class LifeUser(AbstractBaseUser):
         "Is the user a member of staff?"
         # Simplest possible answer: All admins are staff
         return self.is_admin
-
-
-class LifeCard(models.Model):
-    STATUS_INFO = (
-        ('0', 'Active'),
-        ('1', 'Inactive'),
-    )
-
-    TYPE_INFO = (
-        ('W', 'Work'),
-        ('P', 'Play'),
-        ('R', 'Rest'),
-    )
-
-    title = models.CharField(max_length=500)
-    timestamp = models.DateTimeField()
-    status = models.CharField(max_length=1, choices=STATUS_INFO)
-    type = models.CharField(max_length=1, choices=TYPE_INFO)
-
-    # account ID which is same as account_id in life3.user.models.LifeUser
-    account_id = models.CharField(max_length=30, null=False)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    modified_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return 'LifeCard Model : {}, {}, {}, {}'.\
-            format(self.title, self.timestamp, self.type, self.status)
-
-    class Meta:
-        ordering = ['-timestamp']
