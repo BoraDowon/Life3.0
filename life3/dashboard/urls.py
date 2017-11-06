aa66cd4328094397c140c3f3ff20c7867b090db5
akahard2dj <akahard2dj@naver.com>
2017-11-06 11:31:12 +0900

    code cleans

--- life3/dashboard/urls.py
+++ life3/dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^lifecards/$', views.LifeCardList.as_view()),
     url(r'^lifecards/(?P<card_pk>[0-9]+)/$', views.LifeCardDetail.as_view()),
-    #url(r'^lifecards', views.api_lifecards),
-    url(r'^stats', views.api_statistics),
+
+    url(r'^stats/', views.api_statistics),
 ]
