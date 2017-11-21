73d6ae1c7de2c4786e262ba274382bc9f8c062cc
akahard2dj <akahard2dj@naver.com>
2017-11-21 15:41:10 +0900

    deleting an auth-decorator

--- life3/dashboard/custom_middleware.py
+++ life3/dashboard/custom_middleware.py
@@ -36,27 +36,3 @@ class SimpleMiddlerware(object):
     def process_response(self, request, response):
         print('process_response: ', request.path)
         return response
-
-
-def _auth_test_helper(request: HttpRequest):
-    try:
-        access_token = request.META.get('HTTP_AUTHORIZATION').replace('TOKEN ', '')
-        if access_token == '1234':
-            return True
-        else:
-            return False
-    except AttributeError:
-        return False
-
-
-def auth_test(func):
-    @wraps(func)
-    def inner(request, *args, **kwargs):
-        result = _auth_test_helper(request)
-        print(result)
-        if result is not False:
-            return result
-        else:
-            return JsonResponse(({'detail': 'Authentication credentials were not provided.'}), status=403)
-        return func(request, *args, **kwargs)
-    return inner
